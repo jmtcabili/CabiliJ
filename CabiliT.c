64b3a9cfcf7be2bc4a33182000531f7bf5101f0b
Johan Marlo Cabili <jmtcabili@gmail.com>
2023-02-18 21:38:35 +0800

    Created separate file for functions, and working on password input

---
+++ CabiliT.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <windows.h>
+#include "func.c"
+
+
+int
+main()
+{
+    char choice; 
+
+    while (choice != 'e' || choice != 'E')
+    {
+        mainMenu();
+        scanf("%c", &choice);
+
+        switch (choice)
+        {
+            case 'm':
+            case 'M':
+                manageData();
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    system("cls");
+    printf("Thank you for playing!");   
+    
+
+
+    return 0; 
+}
---
+++ func.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef char String30[31];
+
+void mainMenu()
+{
+    printf("\n%14s%s\n\n", " ", "--GenQui--");
+    printf("%8s%s\n", " ", "Welcome to the main menu!");
+    printf("%4s%s", " ", "Choose among the following modes:\n\n");
+    printf("-[M]anage Data (Admin)\n-[P]lay the game (Player)\n");
+    printf("-[E]xit the game\n\n I select: ");
+}
+
+void manageData()
+{
+    char password[] = "poginijohan", input[500];
+    int i; 
+
+    printf("Enter admin password: ");
+
+
+}
